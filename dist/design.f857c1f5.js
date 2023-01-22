8930406b7cc0eb2fab330db75419b64628ea689c
Zeuidon <ishan.upadhyaya01@gmail.com>
2023-01-22 13:07:44 +0530

    changes in index.html and scroll.css

--- dist/design.f857c1f5.js
+++ dist/design.f857c1f5.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2383" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51868" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
